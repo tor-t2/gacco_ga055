bdd5b1c74423b28b5e987e2d24d1b5aeb0089596
Toru <tor.t2.tanaka@gmail.com>
2018-12-21 13:41:04 +0900

    MainFrame 追加

---
+++ gacco/src/MainFrame.java
@@ -0,0 +1,53 @@
+import java.awt.Frame;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+public class MainFrame extends Frame implements WindowListener {
+	/* コンストラクタ */
+	MainFrame(){
+		addWindowListener(this);
+	}
+
+	@Override
+	public void windowOpened(WindowEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void windowIconified(WindowEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void windowActivated(WindowEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
